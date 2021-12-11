6049cfcf2ddcc94ed8b0441fa5d817d02d1242d6
tonynguyenpersonal@gmail.com <J379O4!SBQwm>
2021-12-10 22:20:16 -0800

    This is a test

---
+++ TheProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing Git");
+        }
+    }
+}
